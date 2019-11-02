7174b3c5b0c980f8075612a3132bfe3fa94afe7c
tomaszosk <t.oskroba@icloud.com>
2019-11-02 18:05:33 +0100

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Tomasz Oskroba on 11/2/19.
+//  Copyright © 2019 Tomasz Oskroba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
