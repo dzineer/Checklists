a5c0f726a05d4a84ad259ac2213264e614cc82e0
tomaszosk <t.oskroba@icloud.com>
2019-12-01 19:47:20 +0100

    Detail disclosure buttons present Edit Screen

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -52,6 +52,15 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        let controller = storyboard!.instantiateViewController(withIdentifier: "ListDetailViewController") as! ListDetailViewController
+        controller.delegate = self
+        
+        let checklist = lists[indexPath.row]
+        controller.checklistToEdit = checklist
+        
+        navigationController?.pushViewController(controller, animated: true)
+    }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let checklist = lists[indexPath.row]
