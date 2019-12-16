2de30025438611c94d9a010e185b9d77ec7578db
tomaszosk <t.oskroba@icloud.com>
2019-12-16 22:02:53 +0100

    Saving Data Implemented

--- Checklists/SceneDelegate.swift
+++ Checklists/SceneDelegate.swift
@@ -25,6 +25,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+        saveData()
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
@@ -35,6 +36,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        saveData()
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
@@ -46,8 +48,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        saveData()
     }
 
-
+    // MARK: Helper Methods
+    func saveData() {
+        let navigationController = window!.rootViewController as! UINavigationController
+        let controller = navigationController.viewControllers[0] as! AllListsViewController
+        controller.saveChecklists()
+    }
 }
 
