e59d70699ae3edf0a0ab2b0898a5d5e6e36f9f31
tomaszosk <t.oskroba@icloud.com>
2019-12-16 21:24:13 +0100

    Improved Data Model

--- Checklists/View Controllers/AllListsViewController.swift
+++ Checklists/View Controllers/AllListsViewController.swift
@@ -16,20 +16,26 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Add a placeholder data
-        var list = Checklist(name: "Birthdays")
-        lists.append(list)
-        
-        list = Checklist(name: "Groceries")
-        lists.append(list)
-        
-        list = Checklist(name: "Cool Apps")
-        lists.append(list)
-        
-        list = Checklist(name: "To do")
-        lists.append(list)
-        
+//        var list = Checklist(name: "Birthdays")
+//        lists.append(list)
+//
+//        list = Checklist(name: "Groceries")
+//        lists.append(list)
+//
+//        list = Checklist(name: "Cool Apps")
+//        lists.append(list)
+//
+//        list = Checklist(name: "To do")
+//        lists.append(list)
+//
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+//
+//        for list in lists {
+//            let item = ChecklistItem()
+//            item.text = "Item for \(list.name)"
+//            list.items.append(item)
+//        }
+        loadChecklists()
 
     }
 
@@ -107,6 +113,38 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
                 cell.textLabel!.text = checklist.name
             }
             navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    //MARK:- Data Saving
+    func documentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func dataFilePath() -> URL {
+        return documentsDirectory().appendingPathComponent("Checklists.plist")
+    }
+    
+    func saveChecklists() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(lists)
+            try data.write(to: dataFilePath(), options: Data.WritingOptions.atomic)
+        } catch {
+            print("Error encoding list array: \(error.localizedDescription)")
+        }
+    }
+    
+    func loadChecklists() {
+        let path = dataFilePath()
+        if let data = try? Data(contentsOf: path) {
+            let decoder = PropertyListDecoder()
+            do {
+                lists = try decoder.decode([Checklist].self, from: data)
+            } catch {
+                print("Error decoding list array: \(error.localizedDescription)")
+            }
         }
     }
 }
