ad3802764a8818fe2ce7ea1afba7775a69a1f9f2
tomaszosk <t.oskroba@icloud.com>
2019-12-21 19:18:24 +0100

    First run experience is set

--- Checklists/Model/DataModel.swift
+++ Checklists/Model/DataModel.swift
@@ -14,6 +14,7 @@ class DataModel {
     init() {
         loadChecklists()
         registerDefaults()
+        handleFirstTime()
     }
     
     var indexOfSelectedChecklist: Int {
@@ -27,8 +28,22 @@ class DataModel {
     }
     
     func registerDefaults() {
-        let dictionary = [ "ChecklistIndex": -1 ]
+        let dictionary = [ "ChecklistIndex": -1, "FirstTime": true] as [String: Any]
         UserDefaults.standard.register(defaults: dictionary)
+    }
+    
+    func handleFirstTime() {
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+        
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            
+            indexOfSelectedChecklist = 0
+            userDefaults.set(false, forKey: "FirstTime")
+            userDefaults.synchronize()
+        }
     }
     
     //MARK:- Data Saving
