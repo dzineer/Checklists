ff3c0e787b21ba68409483136fa33a62abb4a678
tomaszosk <t.oskroba@icloud.com>
2020-01-01 18:11:57 +0100

    Each Checklist Item has a unique identifier now

--- Checklists/Model/ChecklistItem.swift
+++ Checklists/Model/ChecklistItem.swift
@@ -12,6 +12,14 @@ class ChecklistItem: NSObject, Codable {
     var text = ""
     var checked = false
 
+    var dueDate = Date()
+    var shouldRemind = false
+    var itemID = -1
+    
+    override init() {
+        super.init()
+        itemID = DataModel.nextChecklistItemID()
+    }
 
     func toggleChecked() {
         checked = !checked
